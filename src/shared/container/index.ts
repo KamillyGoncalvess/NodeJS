390c31bb03716bbf8ac8a00a7883385f2c5b8421
root <root@Vamilly-11.localdomain>
2022-11-29 08:26:05 -0300

    Primeiro commit

---
+++ src/shared/container/index.ts
@@ -0,0 +1,23 @@
+import { container } from "tsyringe";
+
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository"
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository", 
+  CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository", 
+  SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+)

0b5ed06120da172408660b8fcb9524ba7ee49e89
Kamilly <gkamilly1607@outlook.com>
2022-12-01 10:06:06 -0300

    Criação da migration

--- src/shared/infra/typeorm/migrations/1669138065414-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1669138065414-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1669138065414 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",

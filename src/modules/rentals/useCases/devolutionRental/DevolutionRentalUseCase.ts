55c924bcf164124f1f27770c4e589dc1c34e8b9f
KamillyGoncalvess <113532684+KamillyGoncalvess@users.noreply.github.com>
2022-12-04 23:32:47 -0300

    Update DevolutionRentalUseCase.ts

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
@@ -10,6 +10,7 @@ interface IRequest {
   user_id: string;
 }
 
+@injectable()
 class DevolutionRentalUseCase {
   constructor(
   @inject("RentalsRepository")

86d83377be8cc8de252aa606bdc531e0a552c983
Kamilly <gkamilly1607@outlook.com>
2022-12-08 15:22:10 -0300

    Env

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,6 +6,8 @@ import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { ProcessCredentials } from "aws-sdk";
 
 
 container.registerSingleton<IDateProvider>(
@@ -18,7 +20,12 @@ container.registerInstance<IMailProvider>(
  new EtherealMailProvider()
 );
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider
+}
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  S3StorageProvider
+  diskStorage[ProcessCredentials.env.disk]
 )

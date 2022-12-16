2ad455dd0a719e8636d61823b0b5a65264a0f886
Kamilly <gkamilly1607@outlook.com>
2022-12-16 11:01:55 -0300

    Rate Limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,6 +8,7 @@ import swaggerUi from "swagger-ui-express";
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter"
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
@@ -15,6 +16,8 @@ import upload from "@config/upload";
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
